cf442d92b182de868c6ab16f5566cf3c69fc6796
ASHISH RANJAN <100501635+Aashu-2002@users.noreply.github.com>
2024-06-18 23:34:33 +0530

    Update SignUp.jsx

--- frontend/src/pages/signup/SignUp.jsx
+++ frontend/src/pages/signup/SignUp.jsx
@@ -37,7 +37,7 @@ const SignUp = () => {
 						</label>
 						<input
 							type='text'
-							placeholder='John Doe'
+							placeholder='Enter Your Full Name'
 							className='w-full input input-bordered  h-10'
 							value={inputs.fullName}
 							onChange={(e) => setInputs({ ...inputs, fullName: e.target.value })}
